7ba4d727a90545d3d24a7c0f6c78bf08f358a472
Maxime Moutet <55545412+MaximeMoutet13@users.noreply.github.com>
2019-11-17 15:33:24 +0100

    Update modelisation.py

--- modelisation.py
+++ modelisation.py
@@ -4,6 +4,7 @@ __date__ = "08/11/19"
 
 import numpy as np
 import matplotlib.pyplot as plt
+import Paramètres
 from mpl_toolkits.mplot3d import Axes3D
 
 
@@ -11,8 +12,8 @@ def F(t, Y):
     return np.array([[Y[3],
                       Y[4],
                       Y[5],
-                      Y[0] * (Y[4] ** 2 + np.sin(Y[1]) ** 2 * Y[5] ** 2) + G * M / Y[0] ** 2,
-                      -2 * Y[3] * Y[4] + Y[0] * np.sin(Y[1]) * np.cos(Y[1]) * Y[5] ** 2,
+                      Y[0] * (Y[4] ** 2 + np.sin(Y[1]) ** 2 * Y[5] ** 2) - G * M / (Y[0] ** 2),
+                      -(2 * Y[3] * Y[4]) / Y[0] + np.sin(Y[1]) * np.cos(Y[1]) * Y[5] ** 2,
                       -2 * Y[3] * Y[5] / Y[0] - 2 * Y[4] * Y[5] / np.tan(Y[1])]])
 
 
@@ -31,18 +32,16 @@ def euler_explicite(F, tinit, Tfinal, yinit, h):
     return y
 
 
-M = 1
-L = 1
-m = 1
-G = 1
+M = 4e9 * Paramètres.M_Soleil
+G = Paramètres.G_p_MS
 
 t0 = 0
-Tf = 10000
-h = 0.1
+Tf = (153 // 3) * 3.154e+7
+h = 1000
 
-r0 = 10
+r0 = 1000
 theta0 = np.pi / 4
-phi0 = np.pi / 3
+phi0 = np.pi / 4
 
 alpha = np.pi / 4
 beta = np.pi / 4
@@ -59,12 +58,9 @@ N = round((Tf - t0) / h)
 t = t0 + np.arange(N + 1) * h
 
 fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-
-
 x = r_t * np.sin(theta_t) * np.cos(phi_t)
 y = r_t * np.sin(theta_t) * np.sin(phi_t)
 z = r_t * np.cos(theta_t)
-plt.plot(x, y, z)
-Axes3D.contour(ax, x, y, z)
+plt.plot(t, x)
 plt.show()
+
